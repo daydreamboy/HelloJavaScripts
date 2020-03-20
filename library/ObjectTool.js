c347ea39fa65b27043fb6d134d35f69d02ba499f
daydreamboy <wesley4chen@gmail.com>
2020-03-20 20:35:58 +0800

    add code

--- library/ObjectTool.js
+++ library/ObjectTool.js
@@ -1,25 +1,39 @@
 class ObjectTool {
-  /**
-   * Get value from object by key path
-   *
-   * @param {Object} object the object
-   * @param {String} keyPath the key path, e.g. 'foo.bar', 'foo.bar.2'
-   * @see https://stackoverflow.com/a/44627252
-   * @returns Return undefined if not found.
-   */
-  static valueForKeyPath(object, keyPath) {
-    var result = undefined;
+    /**
+     * Get value from object by key path
+     *
+     * @param {Object} object the object
+     * @param {String} keyPath the key path, e.g. 'foo.bar', 'foo.bar.2'
+     *
+     * @returns Return undefined if not found.
+     *
+     * @see https://stackoverflow.com/a/44627252
+     */
+    static valueForKeyPath(object, keyPath) {
+        let result = undefined;
 
-    try {
-      result = keyPath.split('.').reduce((previous, current) => {
-        return previous[current];
-      }, object);
-    } catch (error) {
-      //console.log(error);
+        try {
+            result = keyPath.split('.').reduce((previous, current) => {
+                return previous[current];
+            }, object);
+        } catch (error) {
+            //console.log(error);
+        }
+
+        return result;
     }
 
-    return result;
-  }
+    /**
+     * Check a variable if object
+     *
+     * @param variable
+     * @returns {boolean|boolean}
+     *
+     * @see https://stackoverflow.com/a/8511350
+     */
+    static checkIfObject(variable) {
+        return typeof variable === 'object' && variable !== null;
+    }
 }
 
 export default ObjectTool;
--- library/StringTool.js
+++ library/StringTool.js
@@ -1,52 +1,64 @@
 class StringTool {
-  // @see https://stackoverflow.com/a/5040502
-  static TruncatingStyle = Object.freeze({
-    truncatingNone: 0,
-    truncatingHead: 1,
-    truncatingTrail: 2,
-    truncatingMiddle: 3
-  });
+    // @see https://stackoverflow.com/a/5040502
+    static TruncatingStyle = Object.freeze({
+        truncatingNone: 0,
+        truncatingHead: 1,
+        truncatingTrail: 2,
+        truncatingMiddle: 3
+    });
 
-  /**
-   *
-   * @param {*} string
-   * @param {*} limitedLength
-   * @param {*} separator
-   * @param {*} truncatingStyle
-   * @see https://stackoverflow.com/a/5723274
-   */
-  static truncateString(string, limitedLength, truncatingStyle, separator) {
-    if (string.length <= limitedLength) return string;
-    if (truncatingStyle == this.TruncatingStyle.truncatingNone) return string;
+    /**
+     *
+     * @param {*} string
+     * @param {*} limitedLength
+     * @param {*} separator
+     * @param {*} truncatingStyle
+     * @see https://stackoverflow.com/a/5723274
+     */
+    static truncateString(string, limitedLength, truncatingStyle, separator) {
+        if (string.length <= limitedLength) return string;
+        if (truncatingStyle == this.TruncatingStyle.truncatingNone) return string;
 
-    truncatingStyle = truncatingStyle || this.TruncatingStyle.truncatingTrail;
-    separator = separator || "...";
+        truncatingStyle = truncatingStyle || this.TruncatingStyle.truncatingTrail;
+        separator = separator || "...";
 
-    var separatorLength = separator.length;
-    var showedLength = limitedLength - separatorLength;
+        var separatorLength = separator.length;
+        var showedLength = limitedLength - separatorLength;
 
-    switch (truncatingStyle) {
-      case this.TruncatingStyle.truncatingHead: {
-        return separator + string.substr(string.length - showedLength);
-      }
-      case this.TruncatingStyle.truncatingTrail: {
-        return string.substr(0, showedLength) + separator;
-      }
-      case this.TruncatingStyle.truncatingMiddle: {
-        const frontCharsLength = Math.ceil(showedLength / 2);
-        const backCharsLength = Math.floor(showedLength / 2);
-        return (
-          string.substr(0, frontCharsLength) +
-          separator +
-          string.substr(string.length - backCharsLength)
-        );
-      }
-      case this.TruncatingStyle.truncatingNone:
-      default: {
-        return string;
-      }
+        switch (truncatingStyle) {
+            case this.TruncatingStyle.truncatingHead: {
+                return separator + string.substr(string.length - showedLength);
+            }
+            case this.TruncatingStyle.truncatingTrail: {
+                return string.substr(0, showedLength) + separator;
+            }
+            case this.TruncatingStyle.truncatingMiddle: {
+                const frontCharsLength = Math.ceil(showedLength / 2);
+                const backCharsLength = Math.floor(showedLength / 2);
+                return (
+                    string.substr(0, frontCharsLength) +
+                    separator +
+                    string.substr(string.length - backCharsLength)
+                );
+            }
+            case this.TruncatingStyle.truncatingNone:
+            default: {
+                return string;
+            }
+        }
     }
-  }
+
+    /**
+     * Check a variable if string
+     *
+     * @param variable
+     * @returns {boolean}
+     *
+     * @see https://stackoverflow.com/a/9436948
+     */
+    static checkIfString(variable) {
+        return typeof variable === 'string' || variable instanceof String;
+    }
 }
 
 export default StringTool;
